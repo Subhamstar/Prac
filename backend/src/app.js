63ac88dc67869a8abd1217b3280ceeb307e47d3f
Subhamstar <mr.subhamdas999@gmail.com>
2026-01-19 14:48:33 +0530

    Routing Types

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,18 @@
 import express from "express" ;
 const app=express();
-app.listen(3000,()=>{
+app.get("/",(req,res)=>{
+    res.send({firstName:"Subham",lastName:"Das"});
+})
+app.get("/ab?c",(req,res)=>{   //its works for ab and abc
+    res.send({firstName:"Subham",lastName:"Das"});
+})
+app.get("/ab+c",(req,res)=>{   //its works for abc and abbc , abbbc,abbbbbbbbc
+    res.send({firstName:"Subham",lastName:"Das"});
+})
+app.get("/ab*c",(req,res)=>{   //its works for ac, abc ,abbc , abbbc,abbbbbbbbc   and abgiogoih(anything)cd its aloso works
+    res.send({firstName:"Subham",lastName:"Das"});
+})
+app.listen(7777,()=>{
     console.log("Server starting on localhost 3000 !!");
 }) 
 

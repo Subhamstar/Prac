03793b3f8c43c17eb281c28bf3a49549c40b51d3
Subhamstar <mr.subhamdas999@gmail.com>
2026-01-19 14:51:06 +0530

    Other type

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,9 @@ app.get("/ab+c",(req,res)=>{   //its works for abc and abbc , abbbc,abbbbbbbbc
 app.get("/ab*c",(req,res)=>{   //its works for ac, abc ,abbc , abbbc,abbbbbbbbc   and abgiogoih(anything)cd its aloso works
     res.send({firstName:"Subham",lastName:"Das"});
 })
+app.get("/a/",(req,res)=>{   //its works for parths contains a like only a,ab cab,car,koustav etc anywhere the path contains a 
+    res.send({firstName:"Subham",lastName:"Das"});
+})
 app.listen(7777,()=>{
     console.log("Server starting on localhost 3000 !!");
 }) 
